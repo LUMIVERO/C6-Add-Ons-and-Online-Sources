2c32c8a5382e6567c3fc754b51efe4accfc4c5d8
JanaVotteler <31400472+JanaVotteler@users.noreply.github.com>
2017-09-21 11:19:19 +0200

    Change manifest and resx

--- src/UpdateBibliographicDataFromPubMedSearch/Properties/UpdateBibliographicDataFromPubMedSearchResources.Designer.cs
+++ src/UpdateBibliographicDataFromPubMedSearch/Properties/UpdateBibliographicDataFromPubMedSearchResources.Designer.cs
@@ -19,7 +19,7 @@ namespace SwissAcademic.Addons.UpdateBibliographicDataFromPubMedSearch.Propertie
     // -Klasse über ein Tool wie ResGen oder Visual Studio automatisch generiert.
     // Um einen Member hinzuzufügen oder zu entfernen, bearbeiten Sie die .ResX-Datei und führen dann ResGen
     // mit der /str-Option erneut aus, oder Sie erstellen Ihr VS-Projekt neu.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class UpdateBibliographicDataFromPubMedSearchResources {
@@ -98,7 +98,7 @@ namespace SwissAcademic.Addons.UpdateBibliographicDataFromPubMedSearch.Propertie
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die [en] Abstrakt überschreiben ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die [en] Abstract überschreiben ähnelt.
         /// </summary>
         internal static string OverrideAbstract {
             get {
