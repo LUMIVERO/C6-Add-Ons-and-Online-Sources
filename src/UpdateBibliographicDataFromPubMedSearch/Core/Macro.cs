0cf7eb7ab9cad9f74fba8d5d410900bc434f3a23
Daniel Lutz <daniel.lutz@citavi.com>
2017-10-11 09:47:40 +0200

    add result message

--- src/UpdateBibliographicDataFromPubMedSearch/Core/Macro.cs
+++ src/UpdateBibliographicDataFromPubMedSearch/Core/Macro.cs
@@ -1,4 +1,5 @@
-﻿using SwissAcademic.Citavi;
+﻿using SwissAcademic.Addons.UpdateBibliographicDataFromPubMedSearch.Properties;
+using SwissAcademic.Citavi;
 using SwissAcademic.Citavi.DataExchange;
 using SwissAcademic.Citavi.Metadata;
 using SwissAcademic.Citavi.Shell;
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SwissAcademic.Addons.UpdateBibliographicDataFromPubMedSearch
 {
@@ -14,13 +16,25 @@ namespace SwissAcademic.Addons.UpdateBibliographicDataFromPubMedSearch
     {
         public async static void Run(MainForm mainForm, MacroSettings settings)
         {
-            var referencesWithDoi = mainForm
-                                        .GetFilteredReferences()
-                                        .Where(reference => !string.IsNullOrEmpty(reference.PubMedId))
-                                        .ToList();
+            var referencesWithDoi = mainForm.GetFilteredReferences()
+                                            .Where(reference => !string.IsNullOrEmpty(reference.PubMedId))
+                                            .ToList();
             try
             {
-                await GenericProgressDialog.RunTask(mainForm, RunAsync, Tuple.Create(mainForm.Project, referencesWithDoi, settings));
+                var mergedReferences = await GenericProgressDialog.RunTask(mainForm, RunAsync, Tuple.Create(mainForm.Project, referencesWithDoi, settings));
+
+                if (mergedReferences.Count() != 0)
+                {
+                    if (MessageBox.Show(mainForm, UpdateBibliographicDataFromPubMedSearchResources.ProcessFinishWithChangesMessage.FormatString(mergedReferences.Count()), "Citavi", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                    {
+                        var filter = new ReferenceFilter(mergedReferences, "References with locations from file", false);
+                        mainForm.ReferenceEditorFilterSet.Filters.ReplaceBy(new List<ReferenceFilter> { filter });
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(mainForm, UpdateBibliographicDataFromPubMedSearchResources.ProcessFinishWithoutChangesMessage, "Citavi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
             }
             catch (OperationCanceledException)
             {
@@ -30,12 +44,13 @@ namespace SwissAcademic.Addons.UpdateBibliographicDataFromPubMedSearch
             }
         }
 
-        static async Task RunAsync(Tuple<Project, List<Reference>, MacroSettings> tuple, IProgress<PercentageAndTextProgressInfo> progress, CancellationToken cancellationToken)
+        static async Task<IEnumerable<Reference>> RunAsync(Tuple<Project, List<Reference>, MacroSettings> tuple, IProgress<PercentageAndTextProgressInfo> progress, CancellationToken cancellationToken)
         {
             var identifierSupport = new ReferenceIdentifierSupport();
             var references = tuple.Item2;
             var project = tuple.Item1;
             var settings = tuple.Item3;
+            var mergedReferences = new List<Reference>();
 
             foreach (var reference in references)
             {
@@ -61,8 +76,11 @@ namespace SwissAcademic.Addons.UpdateBibliographicDataFromPubMedSearch
                 if (project.Engine.Settings.BibTeXCitationKey.IsTeXEnabled) reference.BibTeXKey = project.BibTeXKeyAssistant.GenerateKey(reference);
                 if (project.Engine.Settings.BibTeXCitationKey.IsCitationKeyEnabled) reference.CitationKey = project.CitationKeyAssistant.GenerateKey(reference);
 
+                mergedReferences.Add(reference);
                 progress.ReportSafe(100 / references.Count * references.IndexOf(reference));
             }
+
+            return mergedReferences;
         }
     }
 }
