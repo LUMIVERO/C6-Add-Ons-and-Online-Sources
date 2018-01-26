afd2dc2420bd5cf132cf961e5acbe0dcbbeb2543
Daniel Lutz <daniel.lutz@citavi.com>
2018-01-26 14:42:39 +0100

    Change ShowMacroEditor to view without configuration | add SettingsViewAddon

---
+++ src/SettingsView/Dialogs/SettingsViewDialog.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SwissAcademic.Addons.SettingsView
+{
+    partial class SettingsViewDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.lvAddonsSettings = new System.Windows.Forms.ListView();
+            this.chKeys = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chValues = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(576, 393);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Location = new System.Drawing.Point(495, 393);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.BtnExport_Click);
+            // 
+            // lvAddonsSettings
+            // 
+            this.lvAddonsSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvAddonsSettings.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chKeys,
+            this.chValues});
+            this.lvAddonsSettings.GridLines = true;
+            this.lvAddonsSettings.Location = new System.Drawing.Point(12, 12);
+            this.lvAddonsSettings.Name = "lvAddonsSettings";
+            this.lvAddonsSettings.Size = new System.Drawing.Size(639, 375);
+            this.lvAddonsSettings.TabIndex = 2;
+            this.lvAddonsSettings.UseCompatibleStateImageBehavior = false;
+            this.lvAddonsSettings.View = System.Windows.Forms.View.Details;
+            // 
+            // chKeys
+            // 
+            this.chKeys.Width = -1;
+            // 
+            // chValues
+            // 
+            this.chValues.Width = -1;
+            // 
+            // SettingsViewDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 428);
+            this.Controls.Add(this.lvAddonsSettings);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnClose);
+            this.Name = "SettingsViewDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "SettingsViewDialog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.ListView lvAddonsSettings;
+        private System.Windows.Forms.ColumnHeader chKeys;
+        private System.Windows.Forms.ColumnHeader chValues;
+    }
+}
---
+++ src/SettingsView/Dialogs/SettingsViewDialog.cs
@@ -0,0 +1,111 @@
+﻿using SwissAcademic.Addons.SettingsView.Properties;
+using SwissAcademic.Citavi.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwissAcademic.Addons.SettingsView
+{
+    public partial class SettingsViewDialog : Form
+    {
+        #region Constructors
+
+        public SettingsViewDialog(Form owner)
+        {
+            InitializeComponent();
+            Owner = owner;
+            Icon = (Icon)owner.Icon.Clone();
+            Localize();
+            InitializeListView();
+        }
+
+        #endregion
+
+        #region Methods
+
+        void Localize()
+        {
+            btnClose.Text = SettingsViewResources.SettingsViewDialog_Close;
+            btnExport.Text = SettingsViewResources.SettingsViewDialog_Export;
+            Text = SettingsViewResources.SettingsViewDialog_Title;
+            chKeys.Text = SettingsViewResources.SettingsViewDialog_LV_Keys;
+            chValues.Text = SettingsViewResources.SettingsViewDialog_LV_Values;
+        }
+
+        void InitializeListView()
+        {
+            lvAddonsSettings.Items.Clear();
+            foreach (CitaviAddOn addon in Program.AddOnEngine.AddOns)
+            {
+                var index = lvAddonsSettings.Groups.Add(new ListViewGroup(addon.Name, HorizontalAlignment.Left));
+                foreach (var setting in addon.Settings)
+                {
+                    var item = new ListViewItem(new[] { setting.Key, setting.Value });
+                    lvAddonsSettings.Items.Add(item);
+                    item.Group = lvAddonsSettings.Groups[index];
+                }
+            }
+
+            btnExport.Enabled = lvAddonsSettings.Items.Count != 0;
+        }
+
+        void WriteSettingsInFile(string filePath)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (CitaviAddOn addon in Program.AddOnEngine.AddOns)
+            {
+                stringBuilder.AppendLine($"{addon.Name} (ID: {addon.Id} | VERSION: {addon.Version.ToString()})");
+                stringBuilder.AppendLine();
+                foreach (var setting in addon.Settings)
+                {
+                    stringBuilder.AppendLine($"\t{setting.Key}: {setting.Value}");
+                }
+
+                stringBuilder.AppendLine($"\n");
+            }
+
+            File.WriteAllText(filePath, stringBuilder.ToString(), Encoding.UTF8);
+        }
+
+        #endregion
+
+        #region Eventhandlers
+
+        void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        void BtnExport_Click(object sender, EventArgs e)
+        {
+            using (var saveFileDialog = new SaveFileDialog { Filter = SettingsViewResources.SettingsViewDialog_Export_Filter })
+            {
+                if (saveFileDialog.ShowDialog(this) != DialogResult.OK) return;
+
+                try
+                {
+                    WriteSettingsInFile(saveFileDialog.FileName);
+                    if (MessageBox.Show(this, SettingsViewResources.SettingsViewDialog_Export_CompleteMessage, SettingsViewResources.SettingsViewDialog_Export_CompleteTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Information,MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                    {
+                        Process.Start("explorer.exe", Path.GetDirectoryName(saveFileDialog.FileName));
+                    }
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(this, SettingsViewResources.SettingsViewDialog_Export_Exception.FormatSmart(exception.Message), SettingsViewResources.SettingsViewDialog_Export_Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
