03d83a4bc3b36e94c07488366504d4574d67cca7
Daniel Lutz <daniel.lutz@citavi.com>
2018-04-18 16:25:33 +0200

    add experimental version of hiding helb box

---
+++ src/HideHelpBox/HideHelpBoxAddon.cs
@@ -0,0 +1,22 @@
+﻿using SwissAcademic.Citavi.Shell;
+using System.Collections.Generic;
+
+namespace SwissAcademic.Addons.HideHelpBox
+{
+
+    public class HideHelpBoxAddon : CitaviAddOn<MainForm>
+    {
+        public override void OnApplicationIdle(MainForm form)
+        {
+            var openForms = form.OpenForms();
+
+            if (openForms != null)
+            {
+                openForms.FormsOf<KnowledgeItemFileForm>().ForEach(k => k.HelpBox.Width = 0);
+                openForms.FormsOf<KnowledgeItemTextForm>().ForEach(k => k.HelpBox.Width = 0);
+            }
+            base.OnApplicationIdle(form);
+        }
+
+    }
+}
