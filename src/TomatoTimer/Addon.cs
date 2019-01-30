875ef85a2622ae1a977263de908c1a252febde01
Daniel Lutz <daniel.lutz@citavi.com>
2019-01-30 08:30:36 +0100

    add TomatoTimer project

---
+++ src/TomatoTimer/Addon.cs
@@ -0,0 +1,116 @@
+﻿using SwissAcademic.Citavi.Shell;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using SwissAcademic.Addons.TomatoTimer.Properties;
+
+namespace SwissAcademic.Addons.TomatoTimer
+{
+    public class Addon : CitaviAddOn<MainForm>
+    {
+        #region Constants
+
+        const string PanelKey = "Tomato.Panel.{0}";
+
+        #endregion
+
+        #region Fields
+
+        TomatoTimer _tomatoTimer;
+
+        Dictionary<Form, Infragistics.Win.UltraWinStatusBar.UltraStatusPanel> _formsAndPanels;
+
+        #endregion
+
+        #region Constructors
+
+        public Addon()
+        {
+            _tomatoTimer = new TomatoTimer();
+            _formsAndPanels = new Dictionary<Form, Infragistics.Win.UltraWinStatusBar.UltraStatusPanel>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void OnApplicationIdle(MainForm mainForm)
+        {
+            if (_formsAndPanels.ContainsKey(mainForm))
+            {
+                _formsAndPanels[mainForm].Appearance.Image = _tomatoTimer.Image;
+                _formsAndPanels[mainForm].Text = _tomatoTimer.Status;
+                _formsAndPanels[mainForm].ToolTipText = _tomatoTimer.ToolTip;
+            }
+
+            base.OnApplicationIdle(mainForm);
+        }
+
+        public override void OnHostingFormLoaded(MainForm mainForm)
+        {
+            if (!_formsAndPanels.ContainsKey(mainForm) && !mainForm.IsPreviewFullScreenForm)
+            {
+                var statusbar = mainForm.GetStatusBar();
+                var appearance = new Infragistics.Win.Appearance
+                {
+                    Image = TomatoTimerResources.tomato,
+                    TextHAlign = Infragistics.Win.HAlign.Left,
+                    TextVAlign = Infragistics.Win.VAlign.Middle,
+                    Cursor = Cursors.Hand
+                };
+
+                var panel = new Infragistics.Win.UltraWinStatusBar.UltraStatusPanel
+                {
+                    Key = PanelKey,
+                    Appearance = appearance,
+                    Text = TomatoTimerResources.StartMessage
+                };
+
+                statusbar.Panels.Insert(statusbar.Panels.Count - 2, panel);
+                statusbar.PanelClick += Statusbar_PanelClick;
+                _formsAndPanels.Add(mainForm, panel);
+                mainForm.FormClosing += MainForm_FormClosing;
+            }
+            base.OnHostingFormLoaded(mainForm);
+        }
+
+        public override void OnLocalizing(MainForm mainForm)
+        {
+            if (_formsAndPanels.ContainsKey(mainForm))
+            {
+                _formsAndPanels[mainForm].Appearance.Image = _tomatoTimer.Image;
+                _formsAndPanels[mainForm].Text = _tomatoTimer.Status;
+                _formsAndPanels[mainForm].ToolTipText = _tomatoTimer.ToolTip;
+            }
+
+            base.OnLocalizing(mainForm);
+        }
+
+        #endregion
+
+        #region EventHandlers
+
+        void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (sender is MainForm mainForm)
+            {
+                mainForm.FormClosing -= MainForm_FormClosing;
+                if (_formsAndPanels.ContainsKey(mainForm))
+                {
+                    _formsAndPanels.Remove(mainForm);
+                    if (_formsAndPanels.Count == 0 && _tomatoTimer.IsRunning) _tomatoTimer.Stop();
+                }
+            }
+        }
+
+        void Statusbar_PanelClick(object sender, Infragistics.Win.UltraWinStatusBar.PanelClickEventArgs e)
+        {
+            if (_formsAndPanels.ContainsValue(e.Panel))
+            {
+                if (_tomatoTimer.IsRunning) _tomatoTimer.Stop();
+                else _tomatoTimer.Start();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/TomatoTimer/Core/TomatoTimer.cs
@@ -0,0 +1,137 @@
+﻿using Infragistics.Win.Misc;
+using SwissAcademic.Citavi.Shell;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Timers;
+using SwissAcademic.Addons.TomatoTimer.Properties;
+
+namespace SwissAcademic.Addons.TomatoTimer
+{
+    public class TomatoTimer
+    {
+        #region Constants
+
+        const string TomatoTimer_DesktopAlert_Key = "TomatoTimer.Key";
+
+        #endregion
+
+        #region Fields
+
+        Timer _timer;
+        TomatoTimerStates _states;
+
+        #endregion
+
+        #region Constructors
+
+        public TomatoTimer()
+        {
+            _states = new TomatoTimerStates(new StoppedTimer(0))
+            {
+                new RunningTimer(25){ MessageResolver=()=>{return TomatoTimerResources.Message_Break; } },
+                new PausingTimer(5),
+                new RunningTimer(25){ MessageResolver=()=>{return TomatoTimerResources.Message_Break; } },
+                new PausingTimer(5),
+                new RunningTimer(25){ MessageResolver=()=>TomatoTimerResources.FinishMessage},
+                new WalkingTimer(15)
+            };
+
+            IsRunning = false;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsRunning { get; private set; }
+
+        public TimerState State => _timer != null ? _states.Current.State : _states.Default.State;
+
+        public int Minutes { get; private set; }
+
+        public string Status => _timer != null ? (Minutes > 1 ? _states.Current.Status : TomatoTimerResources.LastMinute) : TomatoTimerResources.StartMessage;
+
+        public Image Image => _timer != null ? _states.Current.Image : _states.Default.Image;
+
+        public string ToolTip => _timer != null ? (Minutes > 1 ? TomatoTimerResources.RemainingMinutes.FormatString(Minutes) : TomatoTimerResources.LastMinute) : "TomatoTimer";
+
+        #endregion
+
+        #region EventHandlers
+
+        void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Minutes--;
+            if (Minutes == 0)
+            {
+                ShowMessage(_states.Current.Message);
+                _states.Next();
+                Minutes = _states.Current.Minutes;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Start()
+        {
+            if (IsRunning) return;
+            IsRunning = true;
+
+            Minutes = _states.Current.Minutes;
+
+            _timer = new Timer(60000)
+            {
+                AutoReset = true
+            };
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            if (!IsRunning) return;
+            IsRunning = false;
+
+            Minutes = 0;
+            _timer.Stop();
+            _timer.Elapsed -= _timer_Elapsed;
+            _timer.Dispose();
+            _timer = null;
+            _states.Reset();
+        }
+
+        public void ShowMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message)) return;
+            var mainForm = Program.ActiveProjectShell.PrimaryMainForm;
+
+            mainForm.Invoke((Action)delegate {
+
+                var info = new UltraDesktopAlertShowWindowInfo
+                {
+                    Caption = message,
+                    Image = TomatoTimerResources.pause,
+                    ScreenPosition = ScreenPosition.Center,
+                    Key = TomatoTimer_DesktopAlert_Key,
+                };
+
+                var showDesktopAlertMethod = mainForm
+                                             .GetType()
+                                             .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic).ToList()
+                                             .Where(mi => mi.Name.Equals("ShowDesktopAlert", StringComparison.OrdinalIgnoreCase))
+                                             .Where(mi => mi.GetParameters().Count() == 2)
+                                             .Where(mi => mi.GetParameters()[0].ParameterType.Name.Equals("UltraDesktopAlertShowWindowInfo", StringComparison.OrdinalIgnoreCase) && mi.GetParameters()[1].ParameterType.Name.Equals("Int32", StringComparison.OrdinalIgnoreCase))
+                                             .FirstOrDefault();
+                showDesktopAlertMethod?.Invoke(mainForm, new object[] { info, 3000 });
+
+            });
+        }
+
+        #endregion
+    }
+}
