8113196e5216b3bd204201cc20eb3d394cb3b866
Daniel Lutz <daniel.lutz@citavi.com>
2019-05-14 13:09:47 +0200

    update macro logic

--- src/NormalizeAllCapitalAuthorNames/Core/Macro.cs
+++ src/NormalizeAllCapitalAuthorNames/Core/Macro.cs
@@ -15,50 +15,42 @@ namespace SwissAcademic.Addons.NormalizeAllCapitalAuthorNames
             var counter = 0;
             var project = personList.Project;
 
-            var authors = project.Persons.ToArray();
+            var authors = project.Persons.ToList();
             if (!authors.Any()) return;
 
             foreach (var author in authors)
             {
-
                 var originalAuthorFullName = author.FullName.ToString();
                 var originalAuthorLastName = author.LastName.ToString();
 
-                if (originalAuthorFullName == author.FullName.ToUpper() || (originalAuthorLastName == author.LastName.ToUpper() && normalizeCapitalLastname))
+                if ((!string.IsNullOrEmpty(originalAuthorFullName) && originalAuthorFullName.Equals(originalAuthorFullName.ToUpper(), StringComparison.Ordinal)) || (!string.IsNullOrEmpty(originalAuthorLastName) && originalAuthorLastName.Equals(originalAuthorLastName.ToUpper(), StringComparison.Ordinal) && normalizeCapitalLastname))
                 {
                     counter++;
-                    var authorPrefix = author.Prefix.ToString();
+
                     var authorFirstName = author.FirstName.ToString();
                     var authorMiddleName = author.MiddleName.ToString();
                     var authorLastName = author.LastName.ToString();
+
+                    if (!string.IsNullOrEmpty(authorFirstName)) author.FirstName = authorFirstName.ToLower().ToInitialUpper();
+                    if (!string.IsNullOrEmpty(authorMiddleName)) author.MiddleName = authorMiddleName.ToLower().ToInitialUpper();
+                    if (!string.IsNullOrEmpty(authorLastName)) author.LastName = authorLastName.ToLower().ToInitialUpper();
+
+                    var authorPrefix = author.Prefix.ToString();
                     var authorSuffix = author.Suffix.ToString();
-
-                    var authorFirstNameNormal = authorFirstName.ToLower().ToInitialUpper();
-                    var authorMiddleNameNormal = authorMiddleName.ToLower().ToInitialUpper();
-                    var authorLastNameNormal = authorLastName.ToLower().ToInitialUpper();
-
-                    string authorPrefixNormal;
-                    string authorSuffixNormal;
 
                     if (prefixSuffixFirstCapitalLetter == true)
                     {
-                        authorPrefixNormal = authorPrefix.ToLower().ToInitialUpper();
-                        authorSuffixNormal = authorSuffix.ToLower().ToInitialUpper();
+                        if (!string.IsNullOrEmpty(authorPrefix)) author.Prefix = authorPrefix.ToLower().ToInitialUpper();
+                        if (!string.IsNullOrEmpty(authorSuffix)) author.Suffix = authorSuffix.ToLower().ToInitialUpper();
                     }
                     else
                     {
-                        authorPrefixNormal = authorPrefix.ToLower();
-                        authorSuffixNormal = authorSuffix.ToLower();
+                        if (!string.IsNullOrEmpty(authorPrefix)) author.Prefix = authorPrefix.ToLower();
+                        if (!string.IsNullOrEmpty(authorSuffix)) author.Suffix = authorSuffix.ToLower();
                     }
 
-                    author.Prefix = authorPrefixNormal;
-                    author.FirstName = authorFirstNameNormal;
-                    author.MiddleName = authorMiddleNameNormal;
-                    author.LastName = authorLastNameNormal;
-                    author.Suffix = authorSuffixNormal;
                 }
             }
-
             MessageBox.Show(personList, NormalizeAllCapitalAuthorNamesResources.ResultMessage.FormatString(counter), personList.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
