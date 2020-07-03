0a4eed0ea00f2b668c0f2d657483a62e37b073d1
Daniel Lutz <daniel.lutz@outlook.com>
2020-07-03 15:33:04 +0200

    restructure code add netstandard.dll

---
+++ src/MacroManager/Addon.Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace SwissAcademic.Addons.MacroManagerAddon
+{
+    partial class Addon
+    {
+        const string Key_Button_ShowMacroEditor = "SwissAcademic.Addons.MacroManager.ShowMacroEditor";
+        const string Key_Button_OpenInExplorer = "SwissAcademic.Addons.MacroManager.OpenInExplorer";
+        const string Key_Button_Refresh = "SwissAcademic.Addons.MacroManager.Refresh";
+        internal const string Key_Menu_Directory = "SwissAcademic.Addons.MacroManager.Menu.{0}";
+        internal const string Key_Button_Directory = "SwissAcademic.Addons.MacroManager.Command";
+        const string Key_Menu_MacroManager = "SwissAcademic.Addons.MacroManager.MacroMenu";
+        const string Key_Button_Config = "SwissAcademic.Addons.MacroManager.ConfigCommand";
+        const string Key_MacrosDirectory = "SwissAcademic.Addons.MacroManager.MacrosDirectory";
+    }
+}
--- src/MacroManager/Addon.cs
+++ src/MacroManager/Addon.cs
@@ -10,21 +10,8 @@ using System.Windows.Forms;
 
 namespace SwissAcademic.Addons.MacroManagerAddon
 {
-    public class Addon : CitaviAddOn<MainForm>
+    public partial class Addon : CitaviAddOn<MainForm>
     {
-        #region Constants
-
-        const string Key_Button_ShowMacroEditor = "SwissAcademic.Addons.MacroManager.ShowMacroEditor";
-        const string Key_Button_OpenInExplorer = "SwissAcademic.Addons.MacroManager.OpenInExplorer";
-        const string Key_Button_Refresh = "SwissAcademic.Addons.MacroManager.Refresh";
-        internal const string Key_Menu_Directory = "SwissAcademic.Addons.MacroManager.Menu.{0}";
-        internal const string Key_Button_Directory = "SwissAcademic.Addons.MacroManager.Command";
-        const string Key_Menu_MacroManager = "SwissAcademic.Addons.MacroManager.MacroMenu";
-        const string Key_Button_Config = "SwissAcademic.Addons.MacroManager.ConfigCommand";
-        const string Key_MacrosDirectory = "SwissAcademic.Addons.MacroManager.MacrosDirectory";
-
-        #endregion
-
         #region Fields
 
         MacroEditorForm _editor;
