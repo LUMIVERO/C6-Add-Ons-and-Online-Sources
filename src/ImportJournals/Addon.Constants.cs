d8869c3335d730414cb7efb0224ab059111df6a1
Daniel Lutz <daniel.lutz@outlook.com>
2020-07-03 15:24:23 +0200

    restructure code add netstandard.dll

---
+++ src/ImportJournals/Addon.Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace SwissAcademic.Addons.ImportJournalsAddon
+{
+    partial class Addon
+    {
+        const string Key_Menu_ImportJournals = "SwissAcademic.Addons.ImportJournals.CommandbarMenu";
+        const string Key_Button_ImportByFile = "SwissAcademic.Addons.ImportJournals.CommandbarButtonByFile";
+        const string Key_Button_ImportByPubmed = "SwissAcademic.Addons.ImportJournals.CommandbarButtonByPubmed";
+        const string Key_Button_ImportByWoodward = "SwissAcademic.Addons.ImportJournals.CommandbarButtonByWoodward";
+    }
+}
--- src/ImportJournals/Addon.cs
+++ src/ImportJournals/Addon.cs
@@ -3,19 +3,8 @@ using SwissAcademic.Controls;
 
 namespace SwissAcademic.Addons.ImportJournalsAddon
 {
-    public class Addon : CitaviAddOn<PeriodicalList>
+    public partial class Addon : CitaviAddOn<PeriodicalList>
     {
-        #region Constants
-
-        const string Key_Menu_ImportJournals = "SwissAcademic.Addons.ImportJournals.CommandbarMenu";
-        const string Key_Button_ImportByFile = "SwissAcademic.Addons.ImportJournals.CommandbarButtonByFile";
-        const string Key_Button_ImportByPubmed = "SwissAcademic.Addons.ImportJournals.CommandbarButtonByPubmed";
-        const string Key_Button_ImportByWoodward = "SwissAcademic.Addons.ImportJournals.CommandbarButtonByWoodward";
-
-        #endregion
-
-        #region Methods
-
         public override void OnBeforePerformingCommand(PeriodicalList periodicalList, BeforePerformingCommandEventArgs e)
         {
             e.Handled = true;
@@ -63,27 +52,26 @@ namespace SwissAcademic.Addons.ImportJournalsAddon
             if (menu != null)
             {
                 menu.Text = Properties.Resources.ImportJournalsMenu;
-
-                var button = menu.GetCommandbarButton(Key_Button_ImportByFile);
-                if (button != null)
-                {
-                    button.Text = Properties.Resources.ImportJournalsByFileCommandText;
-                }
-
-                button = menu.GetCommandbarButton(Key_Button_ImportByPubmed);
-                if (button != null)
-                {
-                    button.Text = Properties.Resources.ImportJournalsByPubMedCommandText;
-                }
-
-                button = menu.GetCommandbarButton(Key_Button_ImportByWoodward);
-                if (button != null)
-                {
-                    button.Text = Properties.Resources.ImportJournalsByWoodwardLibraryCommandText;
-                }
             }
-        }
 
-        #endregion
+            var button = menu?.GetCommandbarButton(Key_Button_ImportByFile);
+            if (button != null)
+            {
+                button.Text = Properties.Resources.ImportJournalsByFileCommandText;
+            }
+
+            button = menu?.GetCommandbarButton(Key_Button_ImportByPubmed);
+            if (button != null)
+            {
+                button.Text = Properties.Resources.ImportJournalsByPubMedCommandText;
+            }
+
+            button = menu?.GetCommandbarButton(Key_Button_ImportByWoodward);
+            if (button != null)
+            {
+                button.Text = Properties.Resources.ImportJournalsByWoodwardLibraryCommandText;
+            }
+
+        }
     }
 }
